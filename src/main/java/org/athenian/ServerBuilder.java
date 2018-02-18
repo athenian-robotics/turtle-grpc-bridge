bab5bed00f4d3ace6ff4460cd43d917a4af2a2c6
Turtle1331 <Turtle1331@users.github.com>
2018-02-17 22:12:08 -0800

    Make ServerBuilder.forPort() static

--- src/main/java/org/athenian/ServerBuilder.java
+++ src/main/java/org/athenian/ServerBuilder.java
@@ -2,7 +2,7 @@ package org.athenian;
 
 public abstract class ServerBuilder {
     // Proxy for io.grpc.ServerBuilder.forPort()
-    public io.grpc.ServerBuilder<?> forPort(int port) {
+    public static io.grpc.ServerBuilder<?> forPort(int port) {
         return io.grpc.ServerBuilder.forPort(port);
     }
 }
